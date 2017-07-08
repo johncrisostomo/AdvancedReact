6e44e9764b17170ee21142c1c5d70f292885489e
John Crisostomo <johncrisostomo@protonmail.ch>
2017-07-08 21:44:57 +0800

    renders CommentBox inside the App

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import CommentBox from './comment_box';
 
 export default class App extends Component {
   render() {
-    return <div>React simple starter</div>;
+    return (
+      <div>
+        <CommentBox />
+      </div>
+    );
   }
 }
