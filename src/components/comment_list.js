b29aad40f05cb1d126e99085a42bda4ae226405a
John Crisostomo <jd@snapzio.com>
2017-07-10 07:25:34 +0800

    implements the CommentList component and stubs tests

---
+++ src/components/comment_list.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+const CommentList = () =>
+  <ul className="comment-list">
+    <li>This is the comment list</li>
+  </ul>;
+
+export default CommentList;
